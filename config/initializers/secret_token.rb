adb06a9ff95d25922c3d530cf8fac0f177188420
fentick <fentick_xv@msn.com>
2014-01-05 14:59:27 +0800

    Koosuhi project create

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Koosushi::Application.config.secret_key_base = 'a839f3f6288fad6bf2a6cd03ddb65b51f312a2ebbf78157476b76ef93a32f2309f1ce8acdd86adb34f29d78923e96ec90c9f6ad335ad9365b88f77f60b4eebb8'
